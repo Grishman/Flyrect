8dfb8c8ed12ee4f1e39bb5fdfdbd2dab34a78cb7
Andrew Grishman <grishman4life@gmail.com>
2014-11-09 21:09:11 +0200

    Writing TODOs

--- FlyingRect/src/com/sm/tamplate/DrawView.java
+++ FlyingRect/src/com/sm/tamplate/DrawView.java
@@ -1,16 +1,24 @@
 package com.sm.tamplate;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
 
 	private DrawThread drawThread;
+	Paint p;
+	Rect rect;
 
 	public DrawView(Context context) {
 		super(context);
 		getHolder().addCallback(this);
+		p = new Paint();
+		rect = new Rect();
 
 	}
 
@@ -44,4 +52,82 @@ public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
 		}
 
 	}
+
+	class DrawThread extends Thread {
+
+		private boolean running = false;
+		private SurfaceHolder surfaceHolder;
+
+		public DrawThread(SurfaceHolder surfaceHolder) {
+			this.surfaceHolder = surfaceHolder;
+
+		}
+
+		public void setRunning(boolean running) {
+			this.running = running;
+		}
+
+		// –исовачь
+		// TODO Implement
+		private void doDraw(Canvas canvas) {
+			canvas.drawColor(Color.CYAN);
+			// настройка кисти
+			// красный цвет
+			p.setColor(Color.RED);
+			// толщина линии = 10
+			p.setStrokeWidth(10);
+			p.setStyle(Paint.Style.STROKE);
+			// настройка объекта Rect;
+			// лева€ верхн€€ точка (250,300), нижн€€ права€ (350,500)
+			rect.set(250, 300, 350, 400);
+			canvas.drawRect(rect, p);
+
+		}
+
+		// ќбновл€ет позиции
+		// TODO Implement method
+		private void updatePhysics(Canvas canvas) {
+			int dx = 15;
+			int dy = 15;
+			
+			rect.offset(dx, dy);
+			canvas.drawRect(rect, p);
+		}
+
+		@Override
+		public void run() {
+			Canvas canvas;
+
+			while (running) {
+				canvas = null;
+				try {
+					canvas = surfaceHolder.lockCanvas(null);
+					if (canvas == null)
+						continue;
+					// TODO implement update() and draw()
+
+					doDraw(canvas);
+					updatePhysics(canvas);
+					
+				} finally {
+					if (canvas != null) {
+						surfaceHolder.unlockCanvasAndPost(canvas);
+					}
+				}
+			}
+		}
+	}
+
+	public Object getThread() {
+		// TODO Auto-generated method stub
+		return drawThread;
+	}
+
+	public void pause() {
+		// TODO Implement
+		// synchronized (mSurfaceHolder) {
+		// if (mMode == STATE_RUNNING) setState(STATE_PAUSE);
+		// }
+	}
+
 }
--- FlyingRect/src/com/sm/tamplate/MainActivity.java
+++ FlyingRect/src/com/sm/tamplate/MainActivity.java
@@ -4,10 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-
+	private DrawView rectView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(new DrawView(this));
+	}
+	
+	@Override
+	protected void onPause() {
+		// TODO Implement
+		super.onPause();
+		//rectView.getThread().pause(); // pause game when Activity pauses
 	}
 }
